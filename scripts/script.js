d9e6297c3c707c0c625a365d2c78279b62749bca
Abdul Karim <karim.develops@gmail.com>
2024-09-21 07:58:29 +0500

    Change grid bg color on hover to black

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,19 @@ let gridScale = 10;
 for (let i = 1; i <= gridScale; i++) {
   let gridSqRow = document.createElement("div");
   gridSqRow.classList.add("grid-sq-row");
+
   for (let j = 1; j <= gridScale; j++) {
     let gridSq = document.createElement("div");
     gridSq.classList.add("grid-sq");
+    gridSq.setAttribute("id", "gridSq");
     gridSqRow.appendChild(gridSq);
   }
 
   grid.appendChild(gridSqRow);
 }
+
+grid.addEventListener("mouseover", (e) => {
+  if (e.target.id == "gridSq") {
+    e.target.style.background = "black";
+  }
+});

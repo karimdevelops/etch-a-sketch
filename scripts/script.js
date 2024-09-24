7ed7edff82da537087617a3672cc6b91d5404395
Abdul Karim <karim.develops@gmail.com>
2024-09-24 14:09:53 +0500

    Button to update grid scale

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,42 @@
 let grid = document.querySelector("#grid");
-let gridScale = 10;
+let updateButton = document.querySelector("#updateBtn");
+let gridScale = 16;
+drawGrid(gridScale);
 
-for (let i = 1; i <= gridScale; i++) {
-  let gridSqRow = document.createElement("div");
-  gridSqRow.classList.add("grid-sq-row");
+updateButton.addEventListener("click", () => {
+  gridScale = prompt("Enter a grid scale (Max: 30)");
+  while (gridScale > 30) {
+    gridScale = prompt("Error!\nEnter a grid scale again (Max: 30)");
+  }
+  removeGrid();
+  drawGrid(gridScale);
+});
 
-  for (let j = 1; j <= gridScale; j++) {
-    let gridSq = document.createElement("div");
-    gridSq.classList.add("grid-sq");
-    gridSq.setAttribute("id", "gridSq");
-    gridSqRow.appendChild(gridSq);
+function drawGrid(gridScale) {
+  for (let i = 1; i <= gridScale; i++) {
+    let gridSqRow = document.createElement("div");
+    gridSqRow.classList.add("grid-sq-row");
+
+    for (let j = 1; j <= gridScale; j++) {
+      let gridSq = document.createElement("div");
+      gridSq.classList.add("grid-sq");
+      gridSq.setAttribute("id", "gridSq");
+      gridSqRow.appendChild(gridSq);
+    }
+
+    grid.appendChild(gridSqRow);
   }
 
-  grid.appendChild(gridSqRow);
+  grid.addEventListener("mouseover", (e) => {
+    if (e.target.id == "gridSq") {
+      e.target.style.background = getRandomColor();
+    }
+  });
 }
 
-grid.addEventListener("mouseover", (e) => {
-  if (e.target.id == "gridSq") {
-    e.target.style.background = getRandomColor();
-  }
-});
+function removeGrid() {
+  grid.innerHTML = "";
+}
 
 function getRandomColor() {
   whiteCol = 0xffffff;
